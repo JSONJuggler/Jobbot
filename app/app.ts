47e0f2acd3531daed5a22ca0d54f03ad1b942643
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-02-20 18:37:09 -0600

    formatting files

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
-import { RouteProp } from "@react-navigation/native";
+import {RouteProp} from '@react-navigation/native';
 // import { StackNavigationProp } from "@react-navigation/stack";
-import { MaterialBottomTabNavigationProp } from "@react-navigation/material-bottom-tabs";
-import { ApisauceInstance } from "apisauce";
+import {MaterialBottomTabNavigationProp} from '@react-navigation/material-bottom-tabs';
+import {ApisauceInstance} from 'apisauce';
 
 // export type StackParamList = {
 //   Home: undefined;
@@ -34,27 +34,36 @@ export type TabParamList = {
   Settings: undefined;
 };
 
-export type HomeScreenRouteProp = RouteProp<TabParamList, "Home">;
+export type HomeScreenRouteProp = RouteProp<TabParamList, 'Home'>;
 
-export type HomeScreenNavigationProp = MaterialBottomTabNavigationProp<TabParamList, "Home">;
+export type HomeScreenNavigationProp = MaterialBottomTabNavigationProp<
+  TabParamList,
+  'Home'
+>;
 
 export type HomeScreenProps = {
   route: HomeScreenRouteProp;
   navigation: HomeScreenNavigationProp;
 };
 
-export type LoginScreenRouteProp = RouteProp<TabParamList, "Login">;
+export type LoginScreenRouteProp = RouteProp<TabParamList, 'Login'>;
 
-export type LoginScreenNavigationProp = MaterialBottomTabNavigationProp<TabParamList, "Login">;
+export type LoginScreenNavigationProp = MaterialBottomTabNavigationProp<
+  TabParamList,
+  'Login'
+>;
 
 export type LoginScreenProps = {
   route: LoginScreenRouteProp;
   navigation: LoginScreenNavigationProp;
 };
 
-export type ProfileScreenRouteProp = RouteProp<TabParamList, "Profile">;
+export type ProfileScreenRouteProp = RouteProp<TabParamList, 'Profile'>;
 
-export type ProfileScreenNavigationProp = MaterialBottomTabNavigationProp<TabParamList, "Profile">;
+export type ProfileScreenNavigationProp = MaterialBottomTabNavigationProp<
+  TabParamList,
+  'Profile'
+>;
 
 export type ProfileScreenProps = {
   route: ProfileScreenRouteProp;
@@ -62,6 +71,6 @@ export type ProfileScreenProps = {
 };
 
 export type State = {
-  readonly pages: any
-  readonly api: ApisauceInstance
+  readonly pages: any;
+  readonly api: ApisauceInstance;
 };
