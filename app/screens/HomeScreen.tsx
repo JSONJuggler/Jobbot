f5aca2109ce115e4cdee5f9449aaab95b4d1017c
JSONJuggler <25470092+JSONJuggler@users.noreply.github.com>
2021-02-20 18:13:22 -0600

    fixing file structure

---
+++ app/screens/HomeScreen.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Text, SafeAreaView, StyleSheet, View} from 'react-native';
+import {HomeScreenProps} from '../App';
+import globalStyles, {COLORS} from '../styles/styles';
+
+const HomeScreen = ({navigation}: HomeScreenProps) => {
+  return (
+    <SafeAreaView style={[globalStyles.safeArea, styles.safeArea]}>
+      <View style={styles.container}>
+        <Text style={styles.text}>This is the Home Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+  },
+  text: {
+    color: COLORS.black,
+  },
+  safeArea: {
+    backgroundColor: COLORS.homeBackground,
+  },
+});
+
+export default HomeScreen;
